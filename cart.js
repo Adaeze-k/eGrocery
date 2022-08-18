f0e2058963b4ab043fc4f0f63b042e850e9514af
Adaeze Kalu <adaezemcqueen@gmail.com>
2022-08-18 17:09:11 +0100

    added sidebar cart

---
+++ cart.js
@@ -0,0 +1,11 @@
+var cart_btn = document.querySelector('.cart-btn');
+var cart_sidebar = document.querySelector('.cart-sidebar');
+var close_icon = document.querySelector('.close_icon');
+
+cart_btn.onclick = function(){
+    cart_sidebar.style.right = "0";
+}
+
+close_icon.onclick = function(){
+    cart_sidebar.style.right = "-600px";
+}
